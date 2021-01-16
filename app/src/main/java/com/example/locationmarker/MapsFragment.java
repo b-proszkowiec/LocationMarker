2907e7f96684112fc40cbea921a9d46cfd1c5821
b-proszkowiec <boguslaw.proszkowiec@gmail.com>
2021-01-16 11:55:36 +0100

    Enable map fragment

--- app/src/main/java/com/example/locationmarker/MapsFragment.java
+++ app/src/main/java/com/example/locationmarker/MapsFragment.java
@@ -1,57 +1,149 @@
 package com.example.locationmarker;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+
+import com.example.locationmarker.markers.MarkersContainer;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import android.location.LocationListener;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 
-public class MapsFragment extends Fragment {
+import static android.content.Context.LOCATION_SERVICE;
 
-    private OnMapReadyCallback callback = new OnMapReadyCallback() {
+public class MapsFragment extends Fragment implements LocationListener, OnMapReadyCallback {
+    private static final String TAG = "MapsFragment";
+    private static final float DEFAULT_ZOOM = 17f;
+    private static final double INIT_LOCATION_LAT = 52.22514419;      // Ordona Warszawa
+    private static final double INIT_LOCATION_LON = 20.95346435;
 
-        /**
-         * Manipulates the map once available.
-         * This callback is triggered when the map is ready to be used.
-         * This is where we can add markers or lines, add listeners or move the camera.
-         * In this case, we just add a marker near Sydney, Australia.
-         * If Google Play services is not installed on the device, the user will be prompted to
-         * install it inside the SupportMapFragment. This method will only be triggered once the
-         * user has installed Google Play services and returned to the app.
-         */
-        @Override
-        public void onMapReady(GoogleMap googleMap) {
-            LatLng sydney = new LatLng(-34, 151);
-            googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-            googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-        }
-    };
+    // vars
+    private MapView mapView;
+    private GoogleMap mMap;
+    private Location mLastLocation = null;
+    private FusedLocationProviderClient fusedLocationClient;
+    private LocationManager locationManager;
+    private MarkersContainer markersContainer;
 
-    @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_maps, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        // initialize view
+        View view = inflater.inflate(R.layout.fragment_map, container, false);
+
+        // initialize map fragment
+        SupportMapFragment supportMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.google_map);
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(getContext());
+
+        supportMapFragment.getMapAsync(this);
+
+        return view;
     }
 
     @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        SupportMapFragment mapFragment =
-                (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
-        if (mapFragment != null) {
-            mapFragment.getMapAsync(callback);
+    public void onLocationChanged(Location location) {
+        String text = "Accurancy : " + location.getAccuracy() + "m";
+
+        Toast.makeText(getContext(), text, Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onLocationChanged: location has changed");
+        mLastLocation = location;
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng initLocation = new LatLng(INIT_LOCATION_LAT, INIT_LOCATION_LON);
+
+        mMap = googleMap;
+        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(initLocation, DEFAULT_ZOOM));
+
+        Toast.makeText(getContext(), "Map is ready", Toast.LENGTH_SHORT).show();
+
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
         }
+        fusedLocationClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null) {
+                    Log.d(TAG, "onSuccess: successfully got last location");
+                }
+                mLastLocation = location;
+            }
+        });
+        mMap.setMyLocationEnabled(true);
+        markersContainer.getInstance().setContext(getContext());
+        markersContainer.getInstance().setMap(mMap);
+        locationManager = (LocationManager) getContext().getSystemService(LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        String provider = locationManager.getBestProvider(criteria, true);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000L,0.5f, this);
+
+        UiSettings uiSettings = mMap.getUiSettings();
+        uiSettings.setAllGesturesEnabled(true);
+        uiSettings.setMapToolbarEnabled(true);
+        uiSettings.setZoomControlsEnabled(true);
+
+        googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                // When clicked on map
+                MarkerOptions markerOptions = new MarkerOptions();
+                // set position of marker
+                markerOptions.position(latLng);
+                markerOptions.title(latLng.latitude + " : " + latLng.longitude);
+                // remove all markers
+                mMap.clear();
+                // animate to zoom
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, mMap.getCameraPosition().zoom));
+                // Add marker on map
+                mMap.addMarker(markerOptions);
+
+                Toast.makeText(getContext(), latLng.latitude + " : " + latLng.longitude, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
