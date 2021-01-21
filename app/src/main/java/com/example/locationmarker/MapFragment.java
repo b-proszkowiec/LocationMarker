276e964928f8e4a375e546a5a3cb0eda4d9df96f
b-proszkowiec <boguslaw.proszkowiec@gmail.com>
2021-01-21 10:54:12 +0100

    Fix for new fragment instance after switch in navigation view

--- app/src/main/java/com/example/locationmarker/MapFragment.java
+++ app/src/main/java/com/example/locationmarker/MapFragment.java
@@ -1,15 +1,22 @@
 package com.example.locationmarker;
 
 import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.location.Criteria;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.text.InputType;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import androidx.core.app.ActivityCompat;
@@ -18,7 +25,6 @@ import androidx.fragment.app.Fragment;
 import com.example.locationmarker.markers.MarkersContainer;
 import com.example.locationmarker.surface.SurfaceManager;
 import com.google.android.gms.location.FusedLocationProviderClient;
-import android.location.LocationListener;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -28,30 +34,29 @@ import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.tasks.OnSuccessListener;
 
+
 import static android.content.Context.LOCATION_SERVICE;
 
-public class MapsFragment extends Fragment implements LocationListener, OnMapReadyCallback {
+public class MapFragment extends Fragment implements LocationListener, OnMapReadyCallback {
     private static final String LOG_TAG = "MapsFragment";
     private static final float DEFAULT_ZOOM = 17f;
     private static final double INIT_LOCATION_LAT = 52.22514419;      // Ordona Warszawa
     private static final double INIT_LOCATION_LON = 20.95346435;
 
-    // vars
-    private GoogleMap mMap;
     private static Location mLastLocation = null;
     private FusedLocationProviderClient fusedLocationClient;
-    private LocationManager locationManager;
-    private MarkersContainer markersContainer;
     private View view;
+
+    private static String alertDialogInputText;
+    private static LinearLayout addPointLayer, saveLayer;
+    private static Button addPointButton, stopAddingButton, saveButton, resetButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         // initialize view
-        if (view == null)
-        {
-            view = inflater.inflate(R.layout.fragment_map, container, false);
-        }
+        view = inflater.inflate(R.layout.fragment_map, container, false);
+
         // initialize map fragment
         SupportMapFragment supportMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.google_map);
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(getContext());
@@ -87,14 +92,11 @@ public class MapsFragment extends Fragment implements LocationListener, OnMapRea
     @Override
     public void onMapReady(GoogleMap googleMap) {
         LatLng initLocation = new LatLng(INIT_LOCATION_LAT, INIT_LOCATION_LON);
+        initMapLayer();
 
-        if (googleMap == mMap) {
-            return;
-        }
-
-        mMap = googleMap;
-        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
-        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(initLocation, DEFAULT_ZOOM));
+        // vars
+        googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(initLocation, DEFAULT_ZOOM));
 
         Toast.makeText(getContext(), "Map is ready", Toast.LENGTH_SHORT).show();
 
@@ -117,15 +119,13 @@ public class MapsFragment extends Fragment implements LocationListener, OnMapRea
                 mLastLocation = location;
             }
         });
-        mMap.setMyLocationEnabled(true);
-        MarkersContainer.getInstance().setContext(getContext());
-        MarkersContainer.getInstance().setMap(mMap);
-        locationManager = (LocationManager) getContext().getSystemService(LOCATION_SERVICE);
-        Criteria criteria = new Criteria();
-        String provider = locationManager.getBestProvider(criteria, true);
+        googleMap.setMyLocationEnabled(true);
+        MarkersContainer.setContext(getContext());
+        MarkersContainer.getInstance().setMap(googleMap);
+        LocationManager locationManager = (LocationManager) getContext().getSystemService(LOCATION_SERVICE);
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000L,0.5f, this);
 
-        UiSettings uiSettings = mMap.getUiSettings();
+        UiSettings uiSettings = googleMap.getUiSettings();
         uiSettings.setAllGesturesEnabled(true);
         uiSettings.setMapToolbarEnabled(true);
         uiSettings.setZoomControlsEnabled(true);
@@ -143,5 +143,99 @@ public class MapsFragment extends Fragment implements LocationListener, OnMapRea
             return;
         }
         SurfaceManager.getInstance().finish();
+    }
+
+    public static void resetBottomLayer() {
+        addPointLayer.setVisibility(View.VISIBLE);
+        saveLayer.setVisibility(View.INVISIBLE);
+
+        addPointButton.setVisibility(View.VISIBLE);
+        stopAddingButton.setVisibility(View.INVISIBLE);
+    }
+
+    private void initMapLayer() {
+
+        // initialize custom buttons and layers
+        addPointLayer = getActivity().findViewById(R.id.addPointEndLayer);
+        saveLayer = getActivity().findViewById(R.id.saveResetLayer);
+
+        addPointButton = getActivity().findViewById(R.id.addPointButton);
+        stopAddingButton = getActivity().findViewById(R.id.stopAddingButton);
+        saveButton = getActivity().findViewById(R.id.saveButton);
+        resetButton = getActivity().findViewById(R.id.resetButton);
+        resetBottomLayer();
+
+        addPointButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(LOG_TAG, "onClickAddPointButton: button clicked");
+                int points = adPoint();
+
+                if (points == 3) {
+                    stopAddingButton.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+        stopAddingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(LOG_TAG, "onClickEndButton: button clicked");
+                finish();
+
+                addPointLayer.setVisibility(View.INVISIBLE);
+                saveLayer.setVisibility(View.VISIBLE);
+            }
+        });
+
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(LOG_TAG, "onClickResetButton: button clicked");
+                SurfaceManager.getInstance().reset();
+                resetBottomLayer();
+            }
+        });
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(LOG_TAG, "onClickSaveButton: button clicked");
+
+                Runnable alertDialogRunnable = new Runnable() {
+                    @Override
+                    public void run() {
+                        SurfaceManager.getInstance().save(getContext(), alertDialogInputText);
+                    }
+                };
+                userInput(alertDialogRunnable);
+                resetBottomLayer();
+            }
+        });
+    }
+
+    private void userInput(final Runnable func) {
+        AlertDialog.Builder aBuilder = new AlertDialog.Builder(getContext());
+
+        aBuilder.setTitle("Give a name for new area:");
+        final EditText input = new EditText(getContext());
+        input.setInputType(InputType.TYPE_CLASS_TEXT);
+        aBuilder.setView(input);
+
+        aBuilder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                alertDialogInputText = input.getText().toString();
+                func.run();
+            }
+        });
+        aBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+                alertDialogInputText = "";
+            }
+        });
+        aBuilder.show();
     }
 }
