0b207f6730020f0b208ca30a673f0a8667197291
b-proszkowiec <boguslaw.proszkowiec@gmail.com>
2025-04-02 12:16:17 +0200

    temp3

--- app/src/main/java/com/bpr/pecka/SurfaceDetailsActivity.java
+++ app/src/main/java/com/bpr/pecka/SurfaceDetailsActivity.java
@@ -1,5 +1,6 @@
 package com.bpr.pecka;
 
+import static com.bpr.pecka.constants.LocationMarkerConstants.DEFAULT_ZOOM;
 import static com.bpr.pecka.constants.LocationMarkerConstants.LOCATIONS_ITEM_SELECTED;
 
 import android.os.Bundle;
@@ -10,8 +11,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.bpr.pecka.surface.Surface;
 import com.bpr.pecka.surface.SurfaceManager;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
 
-public class SurfaceDetailsActivity extends AppCompatActivity {
+
+
+public class SurfaceDetailsActivity extends AppCompatActivity  implements OnMapReadyCallback {
+    private Surface surface;
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,11 +34,29 @@ public class SurfaceDetailsActivity extends AppCompatActivity {
         TextView surfaceNameTextView = this.findViewById(R.id.surface_name);
         if (getIntent().hasExtra(LOCATIONS_ITEM_SELECTED)) {
             int itemPosition = getIntent().getIntExtra(LOCATIONS_ITEM_SELECTED, 0);
-            Surface surface = SurfaceManager.getInstance().getSurfaces().get(itemPosition);
-            surfaceNameTextView.setText(surface.getName());
+            this.surface = SurfaceManager.getInstance().getSurfaces().get(itemPosition);
+            surfaceNameTextView.setText(this.surface.getName());
         }
 
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.show_surface_map);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(this);
+        }
 
+    }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+
+        SurfaceManager.getInstance().setLastViewedSurface(surface);
+        SurfaceManager.getInstance().refreshView(true, surface);
+
+        LatLng surfaceCenter = SurfaceManager.getInstance().getSurfaceCenterPoint(surface.convertToLatLngList());
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(surfaceCenter, DEFAULT_ZOOM));
     }
 }
